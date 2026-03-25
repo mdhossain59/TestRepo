2f007e524b04611d634bf910916fbee2b995fe66
mdhossain59 <hossainmd754@gmail.com>
2026-03-25 16:23:30 -0400

    Second change by Hossain

---
+++ fundaments/DemoConditional.java
@@ -0,0 +1,17 @@
+package fundaments;
+
+public class DemoConditional {
+    public static void main(String[] args) {
+        
+    int age = 38;
+
+    // condtion start
+    if (age >= 18){
+        System.err.println("Person can take his own decision");
+    }
+    else{
+        System.out.println("Person still young to take decision");
+    }
+}
+    
+}
--- fundaments/Hello.java
+++ fundaments/Hello.java
@@ -1,3 +1,4 @@
+package fundaments;
 public class Hello {
     public static void main(String[] args) {
         System.err.println("Hello java");
